d104c22b0fd8b72307cf443650f5c1f2d5bb7903
Aqib_Javed <aqibbutt3078@gmail.com>
2019-07-05 16:16:20 +0200

    Intial setup

--- CommandLineDrivenTextSearchEngine/src/com/runner/pkg/MainClass.java
+++ CommandLineDrivenTextSearchEngine/src/com/runner/pkg/MainClass.java
@@ -1,12 +1,26 @@
 package com.runner.pkg;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class MainClass {
 
-	
 	public static void main(String[] args) {
-		
-		
-		
+		if (args.length == 0) {
+			throw new IllegalArgumentException("No directory given to index.");
+		}
+		final File indexableDirectory = new File(args[0]);
+		// TODO: Index all files in indexableDirectory
+		try (Scanner keyboard = new Scanner(System.in)) {
+			while (true) {
+				System.out.print("search> ");
+				final String line = keyboard.nextLine();
+				
+				if(line.equalsIgnoreCase(":quit"))
+					break;
+			}
+		}
+		System.out.println("$ - End program");
 	}
-	
+
 }
